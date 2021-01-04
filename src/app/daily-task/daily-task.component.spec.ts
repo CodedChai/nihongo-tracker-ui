c2932e147242cb1a8fc081c7cb252c57964355bf
Connor Hasting <guardianx515@gmail.com>
2021-01-04 17:05:54 -0600

    Updated angular cli to v 11

--- src/app/daily-task/daily-task.component.spec.ts
+++ src/app/daily-task/daily-task.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DailyTaskComponent } from './daily-task.component';
 
@@ -6,7 +6,7 @@ describe('DailyTaskComponent', () => {
   let component: DailyTaskComponent;
   let fixture: ComponentFixture<DailyTaskComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DailyTaskComponent ]
     })
